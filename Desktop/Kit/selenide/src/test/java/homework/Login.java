c5df62c9f0071c7e3e5ab8ac9dbb8ab216b122a2
Katya Krytsun <kateryna.krytsun@gmail.com>
2017-11-14 00:54:09 +0200

    Homework. Selenide. Login

---
+++ Desktop/Kit/selenide/src/test/java/homework/Login.java
@@ -0,0 +1,36 @@
+package homework;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Login {
+
+    @BeforeClass
+    public void setUp()
+    {
+        ChromeDriverManager.getInstance().setup();
+        Configuration.browser = WebDriverRunner.CHROME;
+        open("https://rozetka.com.ua/");
+
+    }
+
+    @Test
+    public void loginUser() {
+
+        $(By.cssSelector("[name=signin]")).click();
+        $(By.name("login")).val("cath.22@yandex.ua");
+        $(By.xpath("//*[@name='password']")).val("q1a2z3");
+        $(By.xpath("//*[@type='submit']")).click();
+        $(By.xpath("//*[@class='header-user-pre-title']")).shouldHave(text("Здравствуйте"));
+    }
+
+
+}
